139fd3e8c428f25c06db31ec36f6c673ded0377a
Crowdbotics <team@crowdbotics.com>
2023-07-27 01:39:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-recipe-42454",
+  url: "https://holy-recipe-42454.botics.co",
+  api: "https://holy-recipe-42454.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
